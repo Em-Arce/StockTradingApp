161bc7f11797489aefc3ca8c2c820c350f9ac0e2
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-15 15:18:44 +0800

    Adds dependent destory line to prevent orphaned records

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,4 @@
 class Role < ApplicationRecord
-  has_many :role_users
-  has_many :users, through: :role_users
+  has_many :role_users, dependent: :destroy
+  has_many :users, through: :role_users, dependent: :destroy
 end
