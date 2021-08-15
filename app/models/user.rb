c71fff8d275e3e39ee583b1ac60ad6ff9dbdaa26
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-15 15:19:03 +0800

    Adds dependent destory line to prevent orphaned records

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :role_users
-  has_many :roles, through: :role_users
+  has_many :role_users, dependent: :destroy
+  has_many :roles, through: :role_users, dependent: :destroy
 
   VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
 
-  validates :email , presence: true, uniqueness:{case_sensetive:false},
-            format:{with:VALID_EMAIL_REGEX,multiline:true}
+  validates :email , presence: true, uniqueness: { case_sensetive:false },
+            format: { with:VALID_EMAIL_REGEX, multiline:true }
   validates :password, presence: true, length: { minimum: 6 }
+
+  #def role_names=(role)
+  #  roles = Role.where("name IN (?)", role)
+  #  self.roles << roles
+  #end
 end
