708675386101ed8614fb7fd2468c9e5d0b32a183
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-16 21:00:35 +0800

    Adds tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :role_users, dependent: :destroy
-  has_many :roles, through: :role_users, dependent: :destroy
 
   VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
-
+  #ensure that email is present, unique and processed as if case insensitive
   validates :email , presence: true, uniqueness: { case_sensetive:false },
             format: { with:VALID_EMAIL_REGEX, multiline:true }
   validates :password, presence: true, length: { minimum: 6 }
+
+  #METHOD 1 to do role based access/permissions
+  #Add admin, broker and buyer columns as boolean
   #below also validates presense of true or false
   validates :broker, exclusion: [nil]
   validates :buyer, inclusion: { in: [true,false] }
-  #create a method to ensure that both broker and buyer fields are not set to false
+  #use below method to ensure that both broker and buyer fields are not set to false
   validate :broker_and_buyer_fields_cannot_be_false, on: [:create, :update]
 
   def broker_and_buyer_fields_cannot_be_false
@@ -24,10 +25,19 @@ class User < ApplicationRecord
     end
   end
 
-  #get the role/s from form and use it to query in Role Table so that the role_id/s
-  #can be taken and passed when RoleUser entries (user with multiple roles)
-  #are also created when user is created
+  #METHOD 2:  CREATE HAS MANY THROUGH ASSOCIATION BET USER AND ROLE
+  #establish association
+  has_many :role_users, dependent: :destroy
+  has_many :roles, through: :role_users, dependent: :destroy
+
+  #use serialize method to save role_names as array of strings
+  serialize :role_names, Array
+
+  #get the role/s from form and use it to search in Role Table so that the role_id/s
+  #can be taken and passed /used to create RoleUser entries (user with multiple roles)
+  #when user is created
   def role_names=(role)
+    #secure syntax that does not show the actual value/s being searched and compared to names in Role
     roles = Role.where("name IN (?)", role)
     self.roles << roles
   end
