e9c1f5f5cc253dc365a1dff153998b6e242479bf
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-17 17:23:33 +0800

    Adds update, destroy, show, callback and private methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,41 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
   def index
     @users = User.all
   end
 
+  def show
+  end
+
   def edit
   end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+          format.html { redirect_to @user, notice: "#{@user.email} details successfully updated." }
+          format.json { render :show, status: :ok, location: @user }
+      else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_path, notice: "#{@user.email} successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :broker, :buyer)
+    end
 end
