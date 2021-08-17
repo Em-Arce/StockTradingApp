04e73509a72160a164e24f580eb746334e9f2f02
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-17 21:37:18 +0800

    refactor code to reflect permissions in Users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,28 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   let(:user) {User.create!(email: 'test@example.com', role_names: nil,
-                admin:false, broker:true, buyer:true,
+                admin:true, broker:true, buyer:true,
                 password: 'password' )}
 
-
   describe "GET #index" do
-    it "should be a success" do
+    it "should be redirected when user is not admin" do
       get users_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "GET #show" do
-    it "should be a success" do
+    it "should be redirected when user is not admin" do
       get user_path(user.id)
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "GET #edit" do
-    it "should be a success" do
+    it "should be redirected when user is not admin" do
       get edit_user_path(user.id)
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
