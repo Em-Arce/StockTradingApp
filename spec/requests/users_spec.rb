60af9fd06c35c6d889812b8bb093a331be480ac6
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-17 17:25:20 +0800

    Adds request http tests for index, show and edit

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET users_path" do
-    it "works" do
+  let(:user) {User.create!(email: 'test@example.com', role_names: nil,
+                admin:false, broker:true, buyer:true,
+                password: 'password' )}
+
+
+  describe "GET #index" do
+    it "should be a success" do
       get users_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET #show" do
+    it "should be a success" do
+      get user_path(user.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET #edit" do
+    it "should be a success" do
+      get edit_user_path(user.id)
       expect(response).to have_http_status(200)
     end
   end
