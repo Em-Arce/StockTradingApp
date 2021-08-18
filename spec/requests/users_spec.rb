a9f0458f41c7391395a63e633a455e54184c2e90
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-18 16:09:47 +0800

    Adds Faker gem

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,14 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  let(:user) {User.create!(email: 'test@example.com', role_names: nil,
-                admin:true, broker:true, buyer:true,
-                password: 'password' )}
+  let(:current_user) {User.create!(email: 'test@admin.com',
+                      role_names: nil,
+                      admin:true,
+                      broker:true,
+                      buyer:true,
+                      password: 'some_password' )}
+
+  let(:valid_attributes) do
+    {
+      'id'         => '1',
+      'email'      => 'test@subject.com'
+      'role_names' => nil,
+      'admin'      => true,
+      'broker'     => true,
+      'buyer'      => true,
+      'password'   => 'some_password1'
+      'user'       => current_user
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      'id'         => '1',
+      'email'      => ' '
+      'role_names' => nil,
+      'admin'      => true,
+      'broker'     => true,
+      'buyer'      => true,
+      'password'   => 'some_password1'
+      'user'       => current_user
+    }
+  end
 
   describe "GET #index" do
-    it "should be redirected when user is not admin" do
+    it "renders a successful response" do
+      user = User.new(valid_attributes)
+      user.user = current_user
+      user.save
       get users_path
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(200)
     end
   end
 
