e90e5e12150f693994280a746924292283a42e6b
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-18 17:42:55 +0800

    Adds feature test for show user

--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -1,44 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe 'Shows a user', type: :feature do
-  let(:user) {User.create!(email: "jane@doe.com",
-                role_names: nil,
-                admin:true,
-                broker:true,
-                buyer:true,
-                password: "some_password")}
-
-  let(:user1) {User.create!(email: "jane1@doe.com",
-                role_names: nil,
-                admin:false,
-                broker:true,
-                buyer:true,
-                password: "some_password")}
-
-  it "should be success when current user is admin" do
+  it "should be success when user is admin" do
+    user_admin = create(:admin)
+    user1      = create(:user)
     visit root_path
     click_link 'Login'
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: user.password
+    fill_in 'user_email', with: user_admin.email
+    fill_in 'user_password', with: user_admin.password
     click_button 'Log in'
-    visit user_path(id: user.id)
-    if user.admin?
-      expect(current_path).to eq user_path(id: user.id)
-      expect(page).to have_content('Welcome!')
-      expect(page).to have_content("#{user.email}")
+    visit users_path
+    expect(current_path).to eq users_path
+    expect(page).to have_content('Users List')
+    expect(page).to have_content("#{user1.email}")
+    click_link "#{user1.email}"
+    visit user_path(id: user1.id)
+    expect(current_path).to eq user_path(id: user1.id)
+    expect(page).to have_content('Welcome!')
+    expect(page).to have_content("#{user1.email}")
+  end
+
+  it "should be redirected when user is not admin" do
+    user_regular = create(:user)
+    visit root_path
+    click_link 'Login'
+    fill_in 'user_email', with: user_regular.email
+    fill_in 'user_password', with: user_regular.password
+    click_button 'Log in'
+    visit user_path(id: user_regular.id)
+    expect(current_path).to eq static_pages_welcome_path
+    expect(page).to have_content('Unauthorized access')
+  end
+
+  context 'checks for buttons' do
+    it 'Back: should redirect admin to index' do
+      user_admin = create(:admin)
+      user1      = create(:user)
+      visit root_path
+      click_link 'Login'
+      fill_in 'user_email', with: user_admin.email
+      fill_in 'user_password', with: user_admin.password
+      click_button 'Log in'
+      visit user_path(id: user1.id)
+      expect(current_path).to eq user_path(id: user1.id)
+      click_link 'Back'
+      visit users_path
+      expect(current_path).to eq users_path
+      expect(page).to have_content('Users List')
     end
   end
 
-  it "should be redirected when current user is not admin" do
-    visit root_path
-    click_link 'Login'
-    fill_in 'user_email', with: user1.email
-    fill_in 'user_password', with: user1.password
-    click_button 'Log in'
-    visit user_path(id: user1.id)
-    if !user1.admin?
-      expect(current_path).to eq static_pages_welcome_path
-      expect(page).to have_content('Unauthorized access')
+  context 'checks for buttons' do
+    it 'Edit: should redirect admin to edit' do
+      user_admin = create(:admin)
+      user1      = create(:user)
+      visit root_path
+      click_link 'Login'
+      fill_in 'user_email', with: user_admin.email
+      fill_in 'user_password', with: user_admin.password
+      click_button 'Log in'
+      visit user_path(id: user1.id)
+      expect(current_path).to eq user_path(id: user1.id)
+      click_link 'Edit'
+      visit edit_user_path(id: user1.id)
+      expect(current_path).to eq edit_user_path(id: user1.id)
+      expect(page).to have_content('Edit User')
     end
   end
 end
