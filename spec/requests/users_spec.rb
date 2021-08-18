371b1ad42266c3053f399bd2456b39b82aafe16a
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-18 16:11:42 +0800

    Adds login step using devise to pass the requests tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,60 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  let(:current_user) {User.create!(email: 'test@admin.com',
+  let(:user_admin) {User.create!(email: 'test@admin.com',
                       role_names: nil,
                       admin:true,
                       broker:true,
                       buyer:true,
-                      password: 'some_password' )}
+                      password: 'some_password' )
+                    }
 
-  let(:valid_attributes) do
-    {
-      'id'         => '1',
-      'email'      => 'test@subject.com'
-      'role_names' => nil,
-      'admin'      => true,
-      'broker'     => true,
-      'buyer'      => true,
-      'password'   => 'some_password1'
-      'user'       => current_user
-    }
+  before do
+    sign_in user_admin
   end
-
-  let(:invalid_attributes) do
-    {
-      'id'         => '1',
-      'email'      => ' '
-      'role_names' => nil,
-      'admin'      => true,
-      'broker'     => true,
-      'buyer'      => true,
-      'password'   => 'some_password1'
-      'user'       => current_user
-    }
-  end
-
+  #happy paths
   describe "GET #index" do
-    it "renders a successful response" do
-      user = User.new(valid_attributes)
-      user.user = current_user
-      user.save
+    it "should have status 200" do
       get users_path
       expect(response).to have_http_status(200)
     end
   end
 
   describe "GET #show" do
-    it "should be redirected when user is not admin" do
-      get user_path(user.id)
-      expect(response).to have_http_status(302)
+    it "should have status 200" do
+      get user_path(user_admin.id)
+      expect(response).to have_http_status(200)
     end
   end
 
   describe "GET #edit" do
-    it "should be redirected when user is not admin" do
-      get edit_user_path(user.id)
-      expect(response).to have_http_status(302)
+    it "should have status 200" do
+      get edit_user_path(user_admin.id)
+      expect(response).to have_http_status(200)
     end
   end
 end
+
