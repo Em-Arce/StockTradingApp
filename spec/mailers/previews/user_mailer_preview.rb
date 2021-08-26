89a7ae7fe795bd2db1468d232fe08fc5d26bd82f
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-26 21:21:35 +0800

    Adds mailers

---
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,13 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  def welcome_email
+    user = User.last
+    UserMailer.welcome_email(user)
+  end
+
+  def welcome_broker
+    user = User.last
+    UserMailer.welcome_broker(user)
+  end
+end
