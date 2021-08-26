34abe9724e117d194f70a712ce778a88406dd555
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-26 21:20:41 +0800

    Adds user_mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,16 @@
+class UserMailer < ApplicationMailer
+  default from: 'railsnewappnotificationaug2021@gmail.com'
+  layout 'mailer'
+
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Welcome to StockTradingApp')
+  end
+
+  def welcome_broker(user)
+    @user = user
+    mail(to: @user.email, subject: 'Welcome to StockTradingApp')
+  end
+end
+
+
