983fed6be7d7467b3bbc3cb470476fdf100bf399
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-03 22:04:59 +0800

    Adds association to users and trades

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,5 @@
 class Stock < ApplicationRecord
+  has_many :trades, dependent: :destroy
+  has_many :users, through: :trades, dependent: :destroy
   validates :symbol, uniqueness: true, on: :create
 end
