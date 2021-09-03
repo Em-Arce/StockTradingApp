071d29aefef4e222164887b35d905e26f5e7f9b9
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-03 22:03:42 +0800

    Adds stock qty to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,61 +22,71 @@ MSFT = client.quote('MSFT')
 Stock.create!(symbol: MSFT.symbol,
               name: MSFT.company_name,
               latest_price: MSFT.latest_price,
-              latest_time: MSFT.latest_time )
+              latest_time: MSFT.latest_time,
+              quantity: 1000 )
 
 AAPL = client.quote('AAPL')
 Stock.create!(symbol: AAPL.symbol,
               name: AAPL.company_name,
               latest_price: AAPL.latest_price,
-              latest_time: AAPL.latest_time )
+              latest_time: AAPL.latest_time,
+              quantity: 1000 )
 
 AMZN = client.quote('AMZN')
 Stock.create!(symbol:AMZN.symbol,
               name: AMZN.company_name,
               latest_price: AMZN.latest_price,
-              latest_time: AMZN.latest_time )
+              latest_time: AMZN.latest_time,
+              quantity: 1000 )
 #NFLX
 NFLX = client.quote('NFLX')
 Stock.create!(symbol:NFLX.symbol,
               name: NFLX.company_name,
               latest_price: NFLX.latest_price,
-              latest_time: NFLX.latest_time )
+              latest_time: NFLX.latest_time,
+              quantity: 1000 )
 #GOOGL
 GOOGL = client.quote('GOOGL')
 Stock.create!(symbol:GOOGL.symbol,
               name: GOOGL.company_name,
               latest_price: GOOGL.latest_price,
-              latest_time: GOOGL.latest_time )
+              latest_time: GOOGL.latest_time,
+              quantity: 1000 )
 #BRK.B
 BRKB = client.quote('BRK.B')
 Stock.create!(symbol:BRKB.symbol,
               name: BRKB.company_name,
               latest_price: BRKB.latest_price,
-              latest_time: BRKB.latest_time )
+              latest_time: BRKB.latest_time,
+              quantity: 1000 )
 #JPM
 JPM = client.quote('JPM')
 Stock.create!(symbol:JPM.symbol,
               name: JPM.company_name,
               latest_price: JPM.latest_price,
-              latest_time: JPM.latest_time )
+              latest_time: JPM.latest_time,
+              quantity: 1000 )
 #JNJ
 JNJ = client.quote('JNJ')
 Stock.create!(symbol:JNJ.symbol,
               name: JNJ.company_name,
               latest_price: JNJ.latest_price,
-              latest_time: JNJ.latest_time )
+              latest_time: JNJ.latest_time,
+              quantity: 1000 )
 
 #SPGI S&P Global Inc
 SPGI = client.quote('SPGI')
 Stock.create!(symbol:SPGI.symbol,
               name: SPGI.company_name,
               latest_price: SPGI.latest_price,
-              latest_time: SPGI.latest_time )
+              latest_time: SPGI.latest_time,
+              quantity: 1000 )
 
 #CME Chicago Mercantile Exchange
 CME = client.quote('CME')
 Stock.create!(symbol:CME.symbol,
               name: CME.company_name,
               latest_price: CME.latest_price,
-              latest_time: CME.latest_time )
+              latest_time: CME.latest_time,
+              quantity: 1000 )
 #binding.pry
