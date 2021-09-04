243ea1ebdca4cc961c01b25bde98da1f253518da
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-04 12:54:36 +0800

    Adds method to decrease stock qty during buy

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,4 +2,9 @@ class Stock < ApplicationRecord
   has_many :trades, dependent: :destroy
   has_many :users, through: :trades, dependent: :destroy
   validates :symbol, uniqueness: true, on: :create
+
+  def subtract_quantity(number)
+    self.quantity = quantity - number
+    self.save!
+  end
 end
