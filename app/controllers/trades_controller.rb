2740c079645491bbbf7ed4a8fe0a702f78e80a7a
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-09-04 12:53:20 +0800

    Adds code for the full buy feature during create and adds aggregation of data in index method

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -3,6 +3,11 @@ class TradesController < ApplicationController
 def index
   @user = current_user
   @trades = @user.trades ||= nil
+  trades = @trades
+  @formatted_trades = trades.to_a.group_by(&:stock_id).map{|stock_id, trades|{:stock_id => stock_id.to_i,
+    :quantity => trades.sum {|q| q.quantity.to_i} }}
+    #:ave_purchase_price => trades.sum {|p,q| p.purchase_price.to_d}/q.quantity.to_i
+  #binding.pry
 end
 
 def new
@@ -27,11 +32,20 @@ def create
   total_price = stock_price * params[:trade][:quantity].to_i
 
   if @user.cash > total_price
-    #hardcode the latest price not the one entered by user
-    @trade.update!(purchase_price: stock_price)
-    #subtract the total_price from cash
-    @user.update_cash(total_price)
-    #binding.pry
+    if @trade.direction == "buy"
+      #hardcode the latest price not the one entered by user
+      @trade.update!(purchase_price: stock_price)
+
+      #subtract the total_price from cash
+      @user.update_cash(total_price)
+
+      #subtract the qty from stock
+      stock.subtract_quantity(params[:trade][:quantity].to_i)
+      #binding.pry
+      redirect_to user_trades_path(current_user.id), notice: "Trade completed."
+    elsif @trade.direction == "sell"
+      redirect_to user_trades_path(current_user.id), notice: "Watch out for the release of sell feature!"
+    end
   else
     redirect_to user_trades_path(current_user.id), notice: "Insufficient cash. This trade costs $#{(total_price - @user.cash).truncate(2)} more than the cash you have."
   end
@@ -39,6 +53,6 @@ end
 
 private
   def trade_params
-    params.require(:trade).permit(:stock_id, :user_id, :quantity, :purchase_price)
+    params.require(:trade).permit(:stock_id, :user_id, :quantity, :purchase_price, :direction)
   end
 end
