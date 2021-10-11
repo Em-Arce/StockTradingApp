2640c46a37c1479b2baf496c6b90257fa649d544
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-10-11 20:17:54 +0800

    Adds method to get user owned stocks and add stock name and symbol

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -13,5 +13,21 @@ class Trade < ApplicationRecord
     self.save!
   end
 
+  #retrieve stocks owned by user through his trades and add the stock name and symbol for the portfolio page
+  def get_stock_info(user)
+    trades = Trade.where(user_id: user.id)
+    trade_ids = trades.pluck(:stock_id).uniq!
+    #group trades by stock id and get sum of qty
+    trades_summary = trades.to_a.group_by(&:stock_id).map{|stock_id, trades|{:stock_id => stock_id.to_i,
+    :quantity => trades.sum {|q| q.quantity.to_i} }}
 
+    trades_summary.each do |trade|
+      trade[:stock_name] = Stock.find_by(id:trade[:stock_id]).name
+      trade[:stock_symbol] = Stock.find_by(id:trade[:stock_id]).symbol
+    end
+
+    return trades_summary
+    #binding.pry
+    #puts "hello"
+  end
 end
